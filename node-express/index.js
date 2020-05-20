389246a7c1ca435381262309c7cb681105069131
Анастасия Смирнова <pl_nastya@mail.ru>
2020-05-21 00:06:23 +0300

    setup express

---
+++ node-express/index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+// аналог http.createServer
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`)
+})
