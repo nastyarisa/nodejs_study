e70dcab18df37f5791fb62a369a8a01961f8cc55
Анастасия Смирнова <pl_nastya@mail.ru>
2020-05-21 00:56:35 +0300

    setup handlebars

--- node-express/index.js
+++ node-express/index.js
@@ -1,7 +1,31 @@
 const express = require('express');
+const path = require('path');
+const exphbs = require('express-handlebars');
 
-// аналог http.createServer
-const app = express();
+const app = express(); // аналог http.createServer
+
+const hbs = exphbs.create({
+  defaultLayout: 'main', // основной layout
+  extname: 'hbs' // кастомное расширение
+});
+
+// регистрируем hbs как движок для рендеринга html-страниц
+app.engine('hbs', hbs.engine);
+// указываем что надо использовать hbs в express
+app.set('view engine', 'hbs');
+// указываем название папки где будут храниться шаблоны
+app.set('views', 'views');
+
+// метод, который позволяет обрабатывать различные get запросы
+app.get('/', (req, res) => {
+  // res.sendFile(path.join(__dirname, 'views', 'index.html'))
+  res.render('index')
+});
+
+app.get('/about', (req, res) => {
+  // res.sendFile(path.join(__dirname, 'views', 'about.html'))
+  res.render('about')
+});
 
 const PORT = process.env.PORT || 3000;
 
